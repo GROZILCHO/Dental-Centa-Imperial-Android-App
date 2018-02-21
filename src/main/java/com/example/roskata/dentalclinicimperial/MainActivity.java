2e673c0fbe151faf8c061517d9ef4fff7ac1ccaa
GROZILCHO <thespecials@gmail.com>
2018-02-21 09:25:49 +0200

    Add the rest files in my repo after some struggling moments :)

---
+++ src/main/java/com/example/roskata/dentalclinicimperial/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.roskata.dentalclinicimperial;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        setSupportActionBar(myToolbar);
+
+
+        getSupportActionBar().setTitle("Dental Center Imperial");
+        getSupportActionBar().setIcon(getDrawable(R.drawable.logo));
+
+        //View Pager
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(), 2000, 4000);
+
+    }
+
+    public class MyTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (viewPager.getCurrentItem() == 0) {
+                        viewPager.setCurrentItem(1);
+                    } else if (viewPager.getCurrentItem() == 1) {
+                        viewPager.setCurrentItem(2);
+                    } else if (viewPager.getCurrentItem() == 2) {
+                        viewPager.setCurrentItem(3);
+                    } else if (viewPager.getCurrentItem() == 3) {
+                        viewPager.setCurrentItem(4);
+                    } else if (viewPager.getCurrentItem() == 4) {
+                        viewPager.setCurrentItem(5);
+                    } else if (viewPager.getCurrentItem() == 5) {
+                        viewPager.setCurrentItem(6);
+                    } else {
+                        viewPager.setCurrentItem(0);
+                    }
+                }
+            });
+
+        }
+    }
+
+
+}
