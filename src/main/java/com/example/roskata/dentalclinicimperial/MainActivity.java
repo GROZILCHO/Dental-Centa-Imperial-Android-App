f7211e6bf70ef3466639d87e9fc1fbe8ba04c43b
GROZILCHO <thespecials@gmail.com>
2018-02-23 14:34:39 +0200

    Links problems: Some of links doesn't work

--- src/main/java/com/example/roskata/dentalclinicimperial/MainActivity.java
+++ src/main/java/com/example/roskata/dentalclinicimperial/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
